07660d48e1d72c9bfab7730e2010545d2ad79433
vigneshs173 <vickyroman404@gmail.com>
2025-03-26 09:21:29 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        playMusic();
+    }
+
+    private static void playMusic() {
+        System.out.println("Music is playing....");
+    }
+}
